4fe50c035a03bed06c5ec8b8ef327d2496e54983
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-01-18 07:21:45 -0800

    updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-import dis
-import sys
-import types
-
-def print_hidden_names(file_path):
-    with open(file_path, 'rb') as file:
-        code = compile(file.read(), file_path, 'exec')
-
-    for item in code.co_names:
-        if not item.startswith('__'):
-            print(item)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./4-hidden_discovery.py <path_to_hidden_4.pyc>")
-    else:
-        print_hidden_names(sys.argv[1])
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
