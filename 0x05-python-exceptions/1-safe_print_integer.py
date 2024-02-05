93401c55993dd6c151edf04cd30f44bb7b630f67
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-02-05 21:32:25 +0100

    updated

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        int(value)
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
+
