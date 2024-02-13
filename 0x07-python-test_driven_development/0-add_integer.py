89718efa45faa5a952e07019858bfebdde14dcf2
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-02-12 22:33:46 -0800

    updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
+"""Defines an integer addition function."""
+
+
 def add_integer(a, b=98):
-    """
-    Function to add two integers
+    """Return the integer addition of a and b.
 
-    Parameters:
-    a (int or float): first number
-    b (int or float: second number (initialized to 98 if not provided)
-
-    Returns:
-    The sum of a and b in (int)
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-    TypeError: If a or b are not ints or floats
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, (int, float)):
-            raise TypeError('a must be an integer')
-    if not isinstance(b, (int, float)):
-        raise TypeError('b must be an integer')
-
-    return int(a) + int(b)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
+"""Defines a name-printing function."""
+
+
 def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
-    Function that prints My name is <first name> <last name>
-
-    Parameters:
-    first_name: the first name
-    last_name=None: the second name
-
-    Returns:
-    nothing
-
-    Exception:
-    first_name and last_name must be strings otherwise, raise a
-    TypeError exception with the message first_name must be a
-    string or last_name must be a string
-    """
-
     if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-
-    print("My name is {:s} {:s}".format(first_name, last_name or ""))
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
