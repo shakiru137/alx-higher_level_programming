8141749e1876dedcabe68ae2361e845688bd7b09
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-03-26 11:25:19 -0700

    updated

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,11 +1,2 @@
-# Start MySQL in safe mode without password authentication
-sudo mysqld_safe --skip-grant-tables --skip-networking &
-
-# Sleep for a moment to ensure MySQL is fully started
-sleep 5
-
-# Execute MySQL query to list databases
+-- list all databases of the MySQL server
 SHOW DATABASES;
-
-# Stop MySQL safe mode
-sudo killall mysqld_safe
